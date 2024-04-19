e1ad0a9f715c6e7a4330c24f28b196cfba13a702
Prima Aulia Gusta <prima@tablevibe.co>
2024-04-19 16:01:58 +0800

    allow unlike photos at the favourite page

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useAuthContext } from "@/context/AuthContext";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getDoggoPhotosByUserId } from "@/firebase/firestore/doggos";
-import DoggoPhoto from "@/app/components/DoggoPhoto";
-import { futura } from "@/app/ui/fonts";
 import Link from "next/link";
+import { futura } from "@/app/ui/fonts";
+import { useAuthContext } from "@/context/AuthContext";
+import {
+  getDoggoPhotosByUserId,
+  deleteDoggoPhoto,
+} from "@/firebase/firestore/doggos";
+import DoggoPhoto from "@/app/components/DoggoPhoto";
 import Button from "@/app/components/Button";
 
 export default function Page() {
@@ -34,30 +37,41 @@ export default function Page() {
     if (user) {
       fetchDoggoPhotos();
     }
-  }, [user, router]);
+  }, [user]);
+
+  const handleLike = async (photoUrl: string) => {
+    if (savedPhotos.includes(photoUrl)) {
+      await deleteDoggoPhoto(photoUrl, user!.uid);
+      setSavedPhotos(savedPhotos.filter((p) => p !== photoUrl));
+      return;
+    }
+  };
 
   return (
     <>
-      {!savedPhotos.length && (
-        <div>
-          <h2 className={futura.className}>
-            You have not liked any doggos yet. Why? Much wow!
-          </h2>
+      <div>
+        <h2 className={futura.className}>
+          {savedPhotos.length
+            ? "Here are your favourite doggos! 🐶"
+            : "You have not liked any doggos yet. Why? Much wow!"}
+        </h2>
+        {savedPhotos.length ? (
+          <ul className="grid grid-cols-2 gap-3">
+            {savedPhotos.map((photo: string) => (
+              <DoggoPhoto
+                key={photo}
+                photo={photo}
+                saved={true}
+                handleLike={handleLike}
+              />
+            ))}
+          </ul>
+        ) : (
           <Link href="/feed">
             <Button className="mt-4 w-full">View feed</Button>
           </Link>
-        </div>
-      )}
-      <ul className="grid grid-cols-2 gap-3">
-        {savedPhotos.map((photo: string) => (
-          <DoggoPhoto
-            key={photo}
-            photo={photo}
-            saved={true}
-            handleLike={() => {}}
-          />
-        ))}
-      </ul>
+        )}
+      </div>
     </>
   );
 }
